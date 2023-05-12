d022f86d0ffcc42725e2d8beac53156a9a451e92
Андрій <110904791+last-war@users.noreply.github.com>
2023-05-12 09:08:14 +0300

    Update config.py
    add default to settings

--- src/conf/config.py
+++ src/conf/config.py
@@ -2,20 +2,20 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    database_url: str
-    secret_key: str
-    algorithm: str
-    mail_username: str
-    mail_password: str
+    database_url: str = 'postgresql+psycopg2://user:password@localhost:5432/postgres'
+    secret_key: str = 'secret'
+    algorithm: str = 'HS256'
+    mail_username: str = 'username'
+    mail_password: str = 'password'
     mail_from: str = 'PythonStudent@meta.ua'
-    mail_port: int
-    mail_server: str
+    mail_port: int = 465
+    mail_server: str = 'smtp.meta.ua'
     redis_host: str = 'localhost'
     redis_port: int = 6379
-    origins: str
-    cloudinary_name: str
-    cloudinary_api_key: str
-    cloudinary_api_secret: str
+    origins: str = 'http://localhost:3000'
+    cloudinary_name: str = 'name'
+    cloudinary_api_key: str = 'key'
+    cloudinary_api_secret: str = 'secret'
 
     class Config:
         env_file = ".env"
