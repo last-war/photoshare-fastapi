490d8201fc50830a09b4ecc37043dc618f0e4e76
Андрій <110904791+last-war@users.noreply.github.com>
2023-05-13 21:17:54 +0300

    Update cloud_image.py
    combined use for avatar and images

--- src/services/cloud_image.py
+++ src/services/cloud_image.py
@@ -35,20 +35,35 @@ class CloudImage:
         """
         name = hashlib.sha256(str(uuid4()).encode('utf-8')).hexdigest()
         return f"photoshare/{name}"
+    
+    @staticmethod
+    def generate_name_avatar(email: str):
+        """
+        The generate_name_avatar function takes an email address as input and returns a unique avatar name.
+        The function uses the first 12 characters of the SHA256 hash of the email address to generate a unique string.
+        This string is then appended with &quot;web9/&quot; to create a valid S3 bucket key.
+
+        :param email: str: Specify the type of parameter that is expected to be passed into the function
+        :return: A string that is a combination of the prefix &quot;web9/&quot;
+        and a 12-character hash of the email address
+        :doc-author: Trelent
+        """
+        name = hashlib.sha256(email.encode('utf-8')).hexdigest()[:12]
+        return f"web9/{name}"
 
     @staticmethod
-    def upload(file, file_name: str):
+    def upload(file, public_id: str, overwrite=True):
         """
         The upload function takes a file and public_id as arguments.
         The function then uploads the file to Cloudinary using the public_id provided.
         If no public_id is provided, one will be generated automatically.
 
-        :param file: Upload a file to the cloudinary server
-        :param file_name: str: Set the public id of the image
-        :return: A dictionary of data about the uploaded image
+        :param file: Specify the file to be uploaded
+        :param public_id: str: Set the public id of the image
+        :return: A dictionary with the following keys:
         :doc-author: Trelent
         """
-        r = cloudinary.uploader.upload(file, public_id=file_name)
+        r = cloudinary.uploader.upload(file, public_id=public_id, overwrite=overwrite)
         return r
 
     @staticmethod
