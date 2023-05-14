acab130ce1fd7508561dc33a193a1097fd743cb2
Андрій <andrey.maybrodskyy@gmail.com>
2023-05-14 10:53:39 +0300

    add tags with routes

--- src/repository/tags.py
+++ src/repository/tags.py
@@ -3,22 +3,18 @@ from src.database.models import Tag, User, UserRole
 from sqlalchemy.orm import Session
 
 
-async def create_tag(body: TagModel, user: User, db: Session):
+async def create_tag(body: TagModel, db: Session):
     """
     Create a new tag in database just if not exist
 
     :param body: request body containing information about new tag
     :type body: TagModel
-    :param user: check the user have needed role to create
-    :type user: User
     :param db: current session to db
     :type db: Session access to database
     :return: added tag object
     :rtype: Tag | None
     """
-    if user.role not in [UserRole.Admin, UserRole.Moderator, UserRole.User]:
-        return None
-    tag = find_tag(body.tag_name, user, db)
+    tag = find_tag(body.tag_name, db)
     if tag:
         return tag
     tag = Tag(**body.dict())
@@ -28,60 +24,48 @@ async def create_tag(body: TagModel, user: User, db: Session):
     return tag
 
 
-async def edit_tag(body: TagModel, user: User, db: Session) -> Tag | None:
+async def edit_tag(body: TagModel, db: Session) -> Tag | None:
     """
     get tag from database by name
 
     :param body: request body containing information about tag for editing
     :type body: TagModel
-    :param user: check the user have needed role to edit the tag
-    :type user: User
     :param db: current session to db
     :type db: Session access to database
     :return: tag object
     :rtype: Tag | None
     """
-    if user.role not in [UserRole.Admin, UserRole.Moderator]:
-        return None
     tag = db.query(Tag).filter(Tag.tag_name == body.tag_name).first()
     return tag
 
 
-async def find_tag(tag_name: str, user: User, db: Session) -> Tag | None:
+async def find_tag(tag_name: str, db: Session) -> Tag | None:
     """
     get tag from database by name
 
     :param tag_name: name to find
     :type tag_name: str
-    :param user: check the user have needed role to find
-    :type user: User
     :param db: current session to db
     :type db: Session access to database
     :return: tag object
     :rtype: Tag | None
     """
-    if user.role not in [UserRole.Admin, UserRole.Moderator, UserRole.User]:
-        return None
     tag = db.query(Tag).filter(Tag.tag_name == tag_name).first()
     return tag
 
 
-async def delete_tag(tag_name: str, user: User, db: Session) -> Tag | None:
+async def delete_tag(tag_name: str, db: Session) -> Tag | None:
     """
     Delete tag from database just for Administrator role
 
     :param tag_name: name to find tag
     :type tag_name: str
-    :param user: check the user have needed role to delete
-    :type user: User
     :param db: current session to db
     :type db: Session access to database
     :return: The deleted tag if found in database
     :rtype: Tag | None
     """
-    if user.role != UserRole.Admin:
-        return None
-    tag = find_tag(tag_name, user, db)
+    tag = find_tag(tag_name, db)
     db.add(tag)
     if tag:
         db.delete(tag)
--- src/routes/tags.py
+++ src/routes/tags.py
@@ -1,94 +1,87 @@
 from fastapi import APIRouter, Depends, status, HTTPException
 
-from src.database.models import User
+from src.database.models import User, UserRole
 from src.repository import tags as repository_tag
 from src.schemas import TagResponse, TagModel
 from sqlalchemy.orm import Session
 from src.database.db import get_db
-from src.services.auth import auth_service
-
+from src.services.roles import RoleAccess
 
 router = APIRouter(prefix='/tag', tags=['tags'])
 
+allowed_operation_get = RoleAccess([UserRole.Admin, UserRole.Moderator, UserRole.User])
+allowed_operation_post = RoleAccess([UserRole.Admin, UserRole.Moderator, UserRole.User])
+allowed_operation_put = RoleAccess([UserRole.Admin, UserRole.Moderator])
+allowed_operation_delete = RoleAccess([UserRole.Admin])
 
-@router.post("/", response_model=TagResponse, status_code=status.HTTP_201_CREATED)
-async def create_tag(body: TagModel,
-                     cur_user: User = Depends(auth_service.get_current_user), db: Session = Depends(get_db)):
+
+@router.post("/", response_model=TagResponse, dependencies=Depends(allowed_operation_post),
+             status_code=status.HTTP_201_CREATED)
+async def create_tag(body: TagModel, db: Session = Depends(get_db)):
     """
     create new tag from tag_name
 
     :param body: all need field to create
     :type body: TagModel
-    :param cur_user: get the user who is making the request
-    :type cur_user: User
     :param db: current session to db
     :type db: Session access to database
     :return: tag object
     :rtype: Tag | None
     """
-    tag = await repository_tag.create_tag(body, cur_user, db)
+    tag = await repository_tag.create_tag(body, db)
     return tag
 
 
-@router.get("/{tag_name}", response_model=TagResponse)
-async def get_one(tag_name: str,
-                  cur_user: User = Depends(auth_service.get_current_user), db: Session = Depends(get_db)):
+@router.get("/{tag_name}", response_model=TagResponse, dependencies=Depends(allowed_operation_get))
+async def get_one(tag_name: str, db: Session = Depends(get_db)):
     """
     route to get tag object by tag name
 
     :param tag_name: tag name to found
     :type tag_name: str
-    :param cur_user: get the user who is making the request
-    :type cur_user: User
     :param db: current session to db
     :type db: Session access to database
     :return: tag object
     :rtype: Tag | None
     """
-    tag = await repository_tag.find_tag(tag_name, cur_user, db)
+    tag = await repository_tag.find_tag(tag_name, db)
     if tag is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Not found')
     return tag
 
 
-@router.put("/{tag_name}", response_model=TagResponse)
-async def update_tag(body: TagModel, cur_user: User = Depends(auth_service.get_current_user),
-                 db: Session = Depends(get_db)):
+@router.put("/{tag_name}", response_model=TagResponse, dependencies=Depends(allowed_operation_put))
+async def update_tag(body: TagModel, db: Session = Depends(get_db)):
     """
     update tag finded by tag name
 
     :param body: all need field to update
     :type body: TagModel
-    :param cur_user: get the user who is making the request
-    :type cur_user: User
     :param db: current session to db
     :type db: Session access to database
     :return: tag object
     :rtype: Tag | None
     """
 
-    tag = await repository_tag.edit_tag(body, cur_user, db)
+    tag = await repository_tag.edit_tag(body, db)
     if tag is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Not found')
     return tag
 
 
-@router.delete("/{tag_name}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete(tag_name: str, cur_user: User = Depends(auth_service.get_current_user),
-                 db: Session = Depends(get_db)):
+@router.delete("/{tag_name}", dependencies=Depends(allowed_operation_delete), status_code=status.HTTP_204_NO_CONTENT)
+async def delete(tag_name: str, db: Session = Depends(get_db)):
     """
     route to delete tag finded by name
 
     :param tag_name: tag to found
     :type tag_name: str
-    :param cur_user: get the user who is making the request
-    :type cur_user: User
     :param db: current session to db
     :type db: Session access to database
     :return: tag object
     :rtype: Tag | None
     """
-    tag = await repository_tag.delete_tag(tag_name, cur_user, db)
+    tag = await repository_tag.delete_tag(tag_name, db)
     if tag is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Not found')
     return tag
