80d1e0387abda09c13a5e6c278093104a3b01727
Oleh Ovchinnikov <112849429+xoka-pro@users.noreply.github.com>
2023-05-15 01:49:53 +0300

    fix error in roles add ban user add update user add update user by admin

--- src/routes/users.py
+++ src/routes/users.py
@@ -1,12 +1,13 @@
-from fastapi import APIRouter, Depends, status, UploadFile, File
+from fastapi import APIRouter, Depends, status, UploadFile, File, HTTPException
 from sqlalchemy.orm import Session
 
 from src.database.db import get_db
-from src.database.models import User
+from src.database.models import User, UserRole
 from src.repository import users as repository_users
 from src.services.auth import auth_service
-from src.schemas import UserResponse
+from src.schemas import UserResponse, UserModel
 from src.services.cloud_image import CloudImage
+from src.services.roles import RoleAccess
 
 router = APIRouter(prefix="/users", tags=["users"])
 
@@ -41,3 +42,35 @@ async def update_avatar_user(file: UploadFile = File(), current_user: User = Dep
     src_url = CloudImage.get_url_for_avatar(public_id, r)
     user = await repository_users.update_avatar(current_user.email, src_url, db)
     return user
+
+
+@router.put("/update_user", response_model=UserResponse)
+async def update_user(
+        body: UserResponse,
+        user: User = Depends(auth_service.get_current_user),
+        db: Session = Depends(get_db)):
+    user = await repository_users.update_user(body, user, db)
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="NOT_FOUND")
+    return user
+
+
+@router.put("/update_user_by_admin", response_model=UserResponse)
+async def update_user_by_admin(
+        body: UserResponse,
+        user: User = Depends(auth_service.get_current_user),
+        db: Session = Depends(get_db)):
+    user = await repository_users.update_user_by_admin(body, user, db)
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="NOT_FOUND")
+    return user
+
+
+@router.put("/ban_user", response_model=UserResponse, dependencies=[Depends(RoleAccess([UserRole.Admin]))])
+async def ban_user(user_id: int, db: Session = Depends(get_db)):
+    ban = await repository_users.ban_user(user_id, db)
+    if ban is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="NOT_FOUND")
+    return ban
