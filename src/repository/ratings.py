20d316e6272d6e5d34660e5309f7b0da22979357
Андрій <110904791+last-war@users.noreply.github.com>
2023-05-17 16:23:09 +0300

    Add files via upload

---
+++ src/repository/ratings.py
@@ -0,0 +1,77 @@
+from fastapi import HTTPException
+from sqlalchemy.orm import Session
+from sqlalchemy import and_
+from starlette import status
+
+from src.schemas import RatingModel
+from src.database.models import Rating, User, Image
+
+
+async def create_rate(image_id: int, rate: int, db: Session, user: User) -> Rating:
+    """
+    The create_rate function creates a new rate for the image with the given id.
+        Args:
+            image_id (int): The id of the image to be rated.
+            rate (int): The rating value.
+
+    :param image_id: int: Get the image id from the request
+    :param rate: int: Set the rate of the image
+    :param db: Session: Access the database
+    :param user: User: Get the user_id of the logged in user
+    :return: A rating object
+    """
+    is_self_image = db.query(Image).filter(and_(Image.id == image_id, Image.user_id == user.id)).first()
+    already_rated = db.query(Rating).filter(and_(Rating.image_id == image_id, Rating.user_id == user.id)).first()
+    image_exists = db.query(Image).filter(Image.id == image_id).first()
+    if is_self_image:
+        raise HTTPException(status_code=status.HTTP_423_LOCKED, detail="It`s not possible to rate own image.")
+    elif already_rated:
+        raise HTTPException(status_code=status.HTTP_423_LOCKED, detail="It`s not possible to rate twice.")
+    elif image_exists:
+        new_rate = Rating(
+            image_id=image_id,
+            rate=rate,
+            user_id=user.id
+        )
+        db.add(new_rate)
+        db.commit()
+        db.refresh(new_rate)
+        return new_rate
+    
+
+async def delete_rate(rate_id: int, db: Session, user: User) -> Rating | None:
+    """
+    The delete_rate function deletes a rating from the database.
+        Args:
+            rate_id (int): The id of the rating to be deleted.
+            db (Session): A connection to the database.
+
+    :param rate_id: int: Specify the id of the rate to be deleted
+    :param db: Session: Access the database
+    :param user: User: Check if the user is logged in
+    :return: The deleted rate
+    """
+    rate = db.query(Rating).filter(Rating.id == rate_id).first()
+    if rate:
+        db.delete(rate)
+        db.commit()
+    return rate
+
+async def calculate_rating(image_id: int, db: Session, user: User) -> float | None:
+    """
+    The calculate_rating function calculate an average rating of the image.
+        Args:
+            image_id (int): The id of the image for calculating rating.
+            db (Session): A connection to the database.
+
+    :param image_id: int: Specify the id of the image for calculating
+    :param db: Session: Access the database
+    :param user: User: Check if the user is logged in
+    :return: The average rating of the image
+    """
+    image_ratings = db.query(Rating).filter(Rating.image_id == image_id).all()
+    if image_ratings:
+        rates = [rate.rate for rate in image_ratings]
+        rating = round(sum(rates) / len(rates), 2)
+        return rating
+    
